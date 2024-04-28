a01348f061a367233cc5a932406756c4213bab6f
Andrey Nikolaev <nikolaevandronik@yandex.ru>
2024-04-28 22:44:10 +0300

    доабвляем днс запис со статическим айпишником

--- terraform/modules/tf-yc-dns/main.tf
+++ terraform/modules/tf-yc-dns/main.tf
@@ -9,3 +9,11 @@ resource "yandex_dns_zone" "dumpling-zone1" {
   zone    = "dumlping-store24.ru."
   public  = true
 }
+
+resource "yandex_dns_recordset" "rs1" {
+  zone_id = yandex_dns_zone.zone1.id
+  name    = "test.example-public.com."
+  type    = "A"
+  ttl     = 200
+  data    = [var.data]
+}
---
+++ terraform/modules/tf-yc-dns/variables.tf
@@ -0,0 +1,3 @@
+  variable "data" {
+    type = string
+}
